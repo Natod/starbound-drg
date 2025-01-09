30f4b1b6c73ec019634fd5dd7b90cd502b7b0ca5
Mayyyyy <97141450+Natod@users.noreply.github.com>
2025-01-08 21:08:09 -0800

    gold and flaregun

--- scripts/deep_blockeffects.lua
+++ scripts/deep_blockeffects.lua
@@ -10,6 +10,8 @@ function init()
   }
   --]] 
   player.giveEssentialItem("beamaxe","deep_pickaxe")
+  player.giveEssentialItem("wiretool","deep_flaregun")
+  player.giveEssentialItem("painttool","deep_platformgun")
   player.makeTechAvailable("deep_flare")
   player.enableTech("deep_flare")
   player.equipTech("deep_flare")
