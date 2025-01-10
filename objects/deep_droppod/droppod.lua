d35ed30fe04e6f5091bbddcd0a91a98a6f749ec9
12090113 <47487266+12090113@users.noreply.github.com>
2025-01-10 03:32:04 -0800

    Drop pod

---
+++ objects/deep_droppod/droppod.lua
@@ -0,0 +1,17 @@
+require "/scripts/util.lua"
+
+function init()
+  self.detectArea = config.getParameter("detectArea")
+  self.detectArea[1] = object.toAbsolutePosition(self.detectArea[1])
+  self.detectArea[2] = object.toAbsolutePosition(self.detectArea[2])
+end
+
+function update(dt)
+    local allPlayers = world.entityQuery(object.position(), 100, {includedTypes = {"player"}})
+    local inPlayers = world.entityQuery(self.detectArea[1], self.detectArea[2], {
+      includedTypes = {"player"},
+      boundMode = "CollisionArea"
+    })
+
+    object.say(string.format("%s out of %s", #inPlayers, #allPlayers))
+end
