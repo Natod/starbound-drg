86fb081ce7363a019b6c7935dc744287437104e6
Mayyyyy <97141450+Natod@users.noreply.github.com>
2025-01-11 00:00:32 -0800

    dwarf intro quest poof

---
+++ quests/deep_intro.lua
@@ -0,0 +1,35 @@
+require("/quests/scripts/portraits.lua")
+require("/quests/scripts/questutil.lua")
+
+function init()
+end
+
+function questStart()
+  player.giveEssentialItem("inspectiontool", "inspectionmode")
+
+  for _, item in pairs(config.getParameter("skipIntroItems", {})) do
+    player.giveItem(item)
+  end
+  player.giveEssentialItem("beamaxe","deep_pickaxe")
+  player.giveEssentialItem("wiretool","deep_flaregun")
+  player.giveEssentialItem("painttool","deep_platformgun")
+  player.makeTechAvailable("deep_flare")
+  player.enableTech("deep_flare")
+  player.equipTech("deep_flare")
+  player.warp("ownship")
+  quest.complete()
+  return
+
+end
+
+function questComplete()
+  player.setIntroComplete(true)
+
+  questutil.questCompleteActions()
+end
+
+function update(dt)
+end
+
+function uninit()
+end
--- scripts/deep_blockeffects.lua
+++ scripts/deep_blockeffects.lua
@@ -9,12 +9,14 @@ function init()
     [4803] = "wet",
   }
   --]] 
+  --[[
   player.giveEssentialItem("beamaxe","deep_pickaxe")
   player.giveEssentialItem("wiretool","deep_flaregun")
   player.giveEssentialItem("painttool","deep_platformgun")
   player.makeTechAvailable("deep_flare")
   player.enableTech("deep_flare")
   player.equipTech("deep_flare")
+  ]]
 end
 
 function update(dt)
