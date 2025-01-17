baee4bdba31a2cae8a4e14114a4db7dcf553f4b0
Mayyyyy <97141450+Natod@users.noreply.github.com>
2025-01-16 17:12:48 -0800

    missionman tweaks 1

--- stagehands/deep_manager.lua
+++ stagehands/deep_manager.lua
@@ -2,10 +2,18 @@ require "/scripts/util.lua"
 require "/scripts/rect.lua"
 
 function init()
+
+  self.players={}
+
+  playerScan()
+
   sb.logInfo("WOW IT REALLY WORKS") --world.loadUniqueEntity
+  --[[
   for i=0, 50, 1 do  
     spawnWave({60*i,670})
   end
+  ]]
+  sb.logInfo(sb.print(playerPositions()))
 end
 
 
@@ -15,4 +23,25 @@ end
 
 function spawnWave(position)
   world.spawnMonster("iguarmor", position)
+end
+
+function playerScan()
+  local players = world.players()
+  local newPlayers = util.filter(players, function(entityId)
+    return contains(self.players, entityId)
+  end)
+  for _,playerId in pairs(newPlayers) do
+    if self.music then
+      world.sendEntityMessage(playerId, "playAltMusic", self.music, config.getParameter("musicFadeInTime"))
+    end
+  end
+  self.players = players
+end
+
+function playerPositions()
+  local positions = {}
+  for _,player in pairs(self.players) do
+    table.insert(positions, world.entityPosition(player))
+  end
+  return positions
 end
