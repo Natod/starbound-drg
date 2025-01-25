cb96c4b8b1864721e830685086353ee54a741c13
Mayyyyy <97141450+Natod@users.noreply.github.com>
2025-01-25 03:32:20 -0800

    no counter

--- items/scripts/deep_renderammo.lua
+++ items/scripts/deep_renderammo.lua
@@ -2,13 +2,12 @@ require "/scripts/vec2.lua"
 
 function init()
   self.baseNumberPath = config.getParameter("numbersImage", "/interface/ammo/numbers_alt.png")
-  self.counter = 0
 end
 
 function update(dt)
   localAnimator.clearDrawables()
-  self.counter = self.counter + 0.1*dt
-  local numberFrame = string.format("%s:%s", self.baseNumberPath, math.floor(self.counter))
+
+  local numberFrame = string.format("%s:%s", self.baseNumberPath, 0)
   localAnimator.addDrawable({
     image = numberFrame,
     position = vec2.add(activeItemAnimation.ownerAimPosition(), {1,-1}),
