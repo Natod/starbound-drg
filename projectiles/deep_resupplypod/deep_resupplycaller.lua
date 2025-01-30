567202c974fed3ada52dfb9fd992176a3c7cf53c
Mayyyyy <97141450+Natod@users.noreply.github.com>
2025-01-29 21:54:32 -0800

    resupply projectiles

--- projectiles/deep_resupplypod/deep_resupplycaller.lua
+++ projectiles/deep_resupplypod/deep_resupplycaller.lua
@@ -7,17 +7,47 @@ local deep_uninit = uninit or function() end
 function init()
   deep_init()
 
-
+  self.checkDist = config.getParameter("floorCheckDistance", 100)
+  mcontroller.setPosition(checkFloor())
+  self.spawnHeight = config.getParameter("podSpawnHeight", 200)
+  --sb.logInfo(entity.id())
+  for i=0,100 do
+    self.pod = world.spawnProjectile(
+      "deep_resupplypod", 
+      vec2.add(mcontroller.position(), {0, self.spawnHeight-2*i}),
+      entity.id(),
+      nil,
+      nil,
+      {podSpawnHeight = self.spawnHeight, callerID = entity.id()}
+    )
+    if self.pod then break end
+  end
+  
+  message.setHandler("deep_podLanded", function(messageName, isLocalEntity)
+    projectile.die()
+  end)
+  
 end
 
 function update()
   deep_update()
 
-
+  mcontroller.setPosition(checkFloor())
+  world.sendEntityMessage(self.pod, "deep_callerHeight", mcontroller.yPosition())
 end
 
 function uninit()
   deep_uninit()
 
-  
+
+end
+
+function checkFloor()
+  local checkPos = vec2.add(mcontroller.position(), {0, -self.checkDist})
+  local val = world.lineCollision(mcontroller.position(), checkPos, {"block","platform"})
+  if val then
+    return val
+  else
+    return checkPos
+  end
 end
---
+++ projectiles/deep_resupplypod/deep_resupplypod.lua
@@ -0,0 +1,49 @@
+require "/scripts/vec2.lua"
+
+local deep_update = update or function() end
+local deep_init = init or function() end
+local deep_uninit = uninit or function() end
+
+function init()
+  deep_init()
+
+  self.callerHeight = mcontroller.yPosition() - config.getParameter("podSpawnHeight", 200)
+  self.callerID = config.getParameter("callerID")
+
+  message.setHandler("deep_callerHeight", function(messageName, isLocalEntity, value)
+    self.callerHeight = value
+  end)
+  mcontroller.setYVelocity(-25)
+end
+
+function update()
+  deep_update()
+
+  if mcontroller.yPosition() <= self.callerHeight then
+    world.sendEntityMessage(self.callerID, "deep_podLanded")
+    sb.logInfo(sb.print(self.callerID))
+    projectile.die()
+  end
+
+  world.damageTiles(
+    {
+      {world.xwrap(math.floor(mcontroller.xPosition()-1+0.5)), math.floor(mcontroller.yPosition()+0.5)},
+      --{world.xwrap(math.floor(mcontroller.xPosition())), math.floor(mcontroller.yPosition()+0.5)},
+      {world.xwrap(math.floor(mcontroller.xPosition()+0.5)), math.floor(mcontroller.yPosition()+0.5)},
+      --{world.xwrap(math.ceil(mcontroller.xPosition())), math.floor(mcontroller.yPosition()+0.5)},
+      {world.xwrap(math.floor(mcontroller.xPosition()+1+0.5)), math.floor(mcontroller.yPosition()+0.5)}
+    },
+    "foreground",
+    mcontroller.position(),
+    "blockish",
+    999999,
+    99999
+  )
+    --world.placeMaterial()
+end
+
+function uninit()
+  deep_uninit()
+
+  world.spawnVehicle("deep_aquarq",mcontroller.position())
+end
