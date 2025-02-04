859951d29e81c93e20dbcc5360b8d2a7dc12e604
Mayyyyy <97141450+Natod@users.noreply.github.com>
2025-02-04 00:50:21 -0800

    variables renamed

--- scripts/deep_playeranimator.lua
+++ scripts/deep_playeranimator.lua
@@ -5,14 +5,19 @@ local deep_init = init or function() end
 
 function init()
   deep_init()
-  self.baseNumberPath = "/interface/ammo/numberssmall.png"
-  self.charWidth = 0.75
-  self.alignment = {left = 0, center = 1, right = 2}
-  self.itemStatus = {}
+  self.deep_numrender = {
+    baseNumberPath = "/interface/ammo/numberssmall.png",
+    charWidth = 0.75,
+    alignment = {left = 0, center = 1, right = 2}
+  }
+  --self.baseNumberPath  --> self.deep_numrender.baseNumberPath
+  --self.charWidth       --> self.deep_numrender.charWidth
+  --self.alignment       --> self.deep_numrender.alignment
+  self.deep_itemStatus = {}
 
   message.setHandler("deep_changeItemField", function(messageName, isLocalEntity, newStatus) --key, value
-    self.itemStatus = newStatus
-    --self.itemStatus[key] = value
+    self.deep_itemStatus = newStatus
+    --self.deep_itemStatus[key] = value
   end)
 
 end
@@ -24,29 +29,29 @@ function update(dt)
   -- the sludge...
   localAnimator.clearDrawables()
 
-  if self.itemStatus.reserveAmmo 
-  and self.itemStatus.rAmmoDigits
-  and self.itemStatus.rAmmoPos
-  and self.itemStatus.rAmmoColor then
+  if self.deep_itemStatus.reserveAmmo 
+  and self.deep_itemStatus.rAmmoDigits
+  and self.deep_itemStatus.rAmmoPos
+  and self.deep_itemStatus.rAmmoColor then
     drawNum(
-      self.itemStatus.reserveAmmo, 
-      self.itemStatus.rAmmoDigits, 
-      self.itemStatus.rAmmoPos, 
-      self.itemStatus.rAmmoColor, 
-      self.alignment.right
+      self.deep_itemStatus.reserveAmmo, 
+      self.deep_itemStatus.rAmmoDigits, 
+      self.deep_itemStatus.rAmmoPos, 
+      self.deep_itemStatus.rAmmoColor, 
+      self.deep_numrender.alignment.right
     )
   end
 
-  if self.itemStatus.loadedAmmo 
-  and self.itemStatus.lAmmoDigits
-  and self.itemStatus.lAmmoPos
-  and self.itemStatus.lAmmoColor then
+  if self.deep_itemStatus.loadedAmmo 
+  and self.deep_itemStatus.lAmmoDigits
+  and self.deep_itemStatus.lAmmoPos
+  and self.deep_itemStatus.lAmmoColor then
     drawNum(
-      self.itemStatus.loadedAmmo, 
-      self.itemStatus.lAmmoDigits, 
-      self.itemStatus.lAmmoPos, 
-      self.itemStatus.lAmmoColor, 
-      self.alignment.right
+      self.deep_itemStatus.loadedAmmo, 
+      self.deep_itemStatus.lAmmoDigits, 
+      self.deep_itemStatus.lAmmoPos, 
+      self.deep_itemStatus.lAmmoColor, 
+      self.deep_numrender.alignment.right
     )
   end
 
@@ -59,9 +64,9 @@ end
 function drawNum(num, places, offset, color, alignment)
   for i=1,places do
     drawDigit(math.floor((num % 10^(i))/10^(i-1)), vec2.add(offset, {
-      self.charWidth/2 + 
-      self.charWidth*(places-i) - 
-      (self.charWidth*(places))/2*alignment, 
+      self.deep_numrender.charWidth/2 + 
+      self.deep_numrender.charWidth*(places-i) - 
+      (self.deep_numrender.charWidth*(places))/2*alignment, 
       0}), 
       color)
   end
@@ -70,7 +75,7 @@ end
 --draw a single digit
 --args: (single-digit integer, vec2F, color)
 function drawDigit(num, offset, color)
-  local numberFrame = string.format("%s:%s", self.baseNumberPath, num)
+  local numberFrame = string.format("%s:%s", self.deep_numrender.baseNumberPath, num)
   localAnimator.addDrawable({
     image = numberFrame,
     position = offset,
