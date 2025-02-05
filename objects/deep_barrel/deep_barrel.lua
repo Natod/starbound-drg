3caf96ecd8dd3976dbbbb1e8891f8fbc7143fe65
Mayyyyy <97141450+Natod@users.noreply.github.com>
2025-02-04 22:29:38 -0800

    barrel parameters

--- objects/deep_barrel/deep_barrel.lua
+++ objects/deep_barrel/deep_barrel.lua
@@ -1,6 +1,14 @@
 require "/scripts/util.lua"
 
 function init()
-  world.spawnVehicle("deep_barrel", entity.position())
-  object.smash(true)
+  local barrelConfig = config.getParameter("barrelConfig", {})
+  world.spawnVehicle("deep_barrel", entity.position(), {
+    invulnerable = barrelConfig.invulnerable, --true
+    health = barrelConfig.health, --35
+    kickable = barrelConfig.kickable, --true
+    killOnReload = barrelConfig.killOnReload --true
+  })
+  if barrelConfig.smashOnSpawn then --true
+    object.smash(true)
+  end
 end
--- vehicles/deep_barrel/deep_barrel.lua
+++ vehicles/deep_barrel/deep_barrel.lua
@@ -7,15 +7,20 @@ function init(dt)
   self.detectRadius = config.getParameter("detectRadius")
   self.playerPosList = {}
   self.nearbyPlayers = {}
-  storage.health = storage.health or 35
+  storage.health = storage.health or config.getParameter("health", 35)
+  storage.kickedSoundFlag = storage.kickedSoundFlag or false
   
   self.movementSettings = config.getParameter("movementSettings")
-  vehicle.setPersistent(true)
-  vehicle.setDamageTeam({type = "passive"})
-  vehicle.setInteractive(false)
+  self.invulnerable = config.getParameter("invulnerable", false)
+  if self.invulnerable then
+    vehicle.setDamageTeam({type = "ghostly"})
+  else
+    vehicle.setDamageTeam({type = "passive"})
+  end
+  self.kickable = config.getParameter("kickable", false)
 
   animator.setAnimationState("body", "idle")
-  --vehicle.destroy()
+  --
   mcontroller.applyParameters(self.movementSettings)
   storage.kicked = storage.kicked or false
   local startVel = config.getParameter("velocity")
@@ -24,22 +29,46 @@ function init(dt)
     storage.kicked = true
   end
   self.kickForce = config.getParameter("kickForce")
+
+  self.killOnReload = config.getParameter("killOnReload", false)
+  if self.killOnReload then
+    vehicle.setPersistent(false)
+    if storage.reloadFlag then
+      vehicle.destroy()
+    end
+    storage.reloadFlag = true
+  else
+    vehicle.setPersistent(true)
+  end
+
 end
 
 function update(dt)
-   
+  
+  if not storage.kickedSoundFlag then
+    animator.playSound("smash")
+    storage.kickedSoundFlag = true
+  end
   local driver = vehicle.entityLoungingIn("seat")
   if driver then
     -- called when you first get in 
     if self.lastDriver == nil then
       --match the index of the driver's entityID to their position
-      for i,p in pairs(self.nearbyPlayers) do
-        if driver == p then
-          local dir = vec2.norm(world.distance(mcontroller.position(), vec2.add(self.playerPosList[p][2],{0,-5})))
-          world.spawnVehicle("deep_barrel", mcontroller.position(), {velocity=vec2.add(vec2.mul(dir, self.kickForce), mcontroller.velocity())})
-          mcontroller.setPosition(vec2.add(self.playerPosList[p][2],{0,-2.65}))
-          vehicle.destroy()
-          break
+      if self.kickable then
+        for i,p in pairs(self.nearbyPlayers) do
+          if driver == p then
+            local dir = vec2.norm(world.distance(mcontroller.position(), vec2.add(self.playerPosList[p][2],{0,-5})))
+            world.spawnVehicle("deep_barrel", mcontroller.position(), {
+              velocity = vec2.add(vec2.mul(dir, self.kickForce), mcontroller.velocity()),
+              invulnerable = self.invulnerable,
+              health = storage.health,
+              kickable = self.kickable,
+              killOnReload = self.killOnReload
+            })
+            mcontroller.setPosition(vec2.add(self.playerPosList[p][2],{0,-2.65}))
+            vehicle.destroy()
+            break
+          end
         end
       end
     end
@@ -91,8 +120,10 @@ function update(dt)
     if spinny then
       local rand = math.random()
       animator.rotateTransformationGroup("rotate", ((rand-0.5)*2)*rand * math.pi/3, {0,1.5})
+      animator.playSound("smash")
     else
       animator.resetTransformationGroup("rotate")
+      --animator.stopAllSounds("smash")
     end
   end
 end
